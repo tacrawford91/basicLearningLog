0010ddca20ee4861072de5934f2bd3ac78be320c
Troy Crawford <tacrawford91@gmail.com>
2018-06-24 16:21:18 -0500

    basic log complete

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,22 @@
-// placeholder
+// adding new entry
+
+
+$('#add').on('click', () => {
+    let newEntry = {
+        day: $('#day').val().trim(),
+        skill: $('#skill').val().trim(),
+        summary: $('#summary').val().trim(),
+        description: $('#description').val().trim(),
+        imageURL: $('#imageURL').val().trim(),
+        language: $('#language').val().trim()
+    }
+
+    console.log('new entry', newEntry);
+    
+    
+    $.ajax({
+        method: 'POST',
+        url:'/api/newEntry',
+        data: newEntry
+    }).then((res)=> {console.log('data added',res)})
+})
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,16 +7,9 @@
     //Create New Entry
     router.post("/newEntry", (req,res) => {
         console.log(req.body);
-        db.Entry.create(req.body).then((newEntry) => {res.send("new entry added")}).catch((err) => {if (err) throw err})
+        db.Entry.create(req.body).then((newEntry) => {res.send(newEntry)}).catch((err) => {if (err) throw err})
     });
 
-    // //Get all Entries
-    // router.get("/allEntries", (req,res) => {
-    //     db.Entry.findAll({}).then( (data)  => {
-    //         console.log(data);
-    //         res.json(data);
-    //     });
-    // });
 
 
     module.exports = router;
