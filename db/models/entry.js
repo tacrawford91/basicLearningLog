bb6ce635b6ebb906676e10b82db061f3dfdfce1b
Troy Crawford <tacrawford91@gmail.com>
2018-06-28 22:26:23 -0500

    fix string length

--- db/models/entry.js
+++ db/models/entry.js
@@ -2,7 +2,7 @@ module.exports = function(sequelize, DataTypes) {
     var Entry = sequelize.define("Entry", {
       day: DataTypes.STRING,
       skill: DataTypes.STRING,
-      summary: DataTypes.STRING(5000),
+      summary: DataTypes.STRING(1234),
       description: DataTypes.STRING,
       imageURL: DataTypes.STRING,
       language: DataTypes.STRING
