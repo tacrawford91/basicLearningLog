8fa3df2796c193a7ccd998cf3c88fe0ddf367dc3
Troy Crawford <tacrawford91@gmail.com>
2018-07-15 13:31:52 -0500

    added moment

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,15 +3,31 @@
     const router = require('express').Router();
     // Requiring models
     const db = require("../db/models");
+    //moment
+    const moment = require('moment')
 
 
     router.get("/", (req,res) => {
         var hbsObject;
         db.Entry.findAll({order:[['createdAt','DESC']]})
-        .then((allEntries) => {
-            // console.log('all entries', allEntries.dataValues)
+        .then((data) => {
+            let newData = [];
+            data.forEach( data => {
+                let htmlData = {
+                    id: data.id,
+                    day: moment(data.day).format("dddd, MMMM Do YYYY"),
+                    skill: data.skill, 
+                    summary: data.summary,
+                    description: data.description,
+                    imageURL: data.imageURL,
+                    language: data.language,
+                    createdAt: data.createdAt,
+                    updatedAt: data.updatedAt
+                }
+                newData.push(htmlData);
+            })
             hbsObject = {
-                data: allEntries
+                data: newData
             };
             console.log('handlebar object', hbsObject);
         }).then( () => {res.render("index", hbsObject)}).catch((err) => console.log(`rendering THE IS ERROR IS THE FOLLOWING: ${err}`));      
