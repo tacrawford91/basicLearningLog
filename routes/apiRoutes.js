88d99130331d50a91750d5796ddab6a0393165bd
Troy Crawford <tacrawford91@gmail.com>
2018-09-02 10:38:42 -0500

    added some additional routes to expand use of API

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,26 @@
 (()=>{
     //require express & setup Router
-    const router = require("express").Router();
+    const router = require('express').Router();
     // Requiring models
-    const db = require("../db/models");
+    const db = require('../db/models');
 
+
+   //get One
+   router.get('/get/:id', (req,res) => {
+       db.Entry.findById(req.params.id)
+       .then((data) => res.json(data));
+   });
+
+    //get all entries
+    router.get('/getAll', (req,res) => {
+        db.Entry.findAll({order:[['createdAt','DESC']]})
+        .then((data) => {
+            res.json(data);
+        })
+   });
+   
     //Create New Entry
-    router.post("/newEntry", (req,res) => {
-        console.log(req.body);
+    router.post('/newEntry', (req,res) => {
         db.Entry.create(req.body).then((newEntry) => {res.send(newEntry)}).catch((err) => {if (err) throw err})
     });
 
