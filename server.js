9e58150365c2c5a652378a8f54bfdb8fa29f174d
Troy Crawford <tacrawford91@gmail.com>
2018-09-02 15:20:00 -0500

    unfix

--- server.js
+++ server.js
@@ -17,11 +17,11 @@
 
         const routes = require('./routes');
         //Use routes
-        app.use(function(req, res, next) {
-            res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            next();
-        });
+        // app.use(function(req, res, next) {
+        //     res.header("Access-Control-Allow-Origin", "*");
+        //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        //     next();
+        // });
         app.use(routes);
 
 
